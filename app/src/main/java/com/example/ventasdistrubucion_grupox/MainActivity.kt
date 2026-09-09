1d34e2258040252824fbb99f2dc7926b97099f00
José Tapia <jos.tapiab@duocuc.cl>
2026-09-09 16:56:43 -0300

    Commit Inicial

---
+++ app/src/main/java/com/example/ventasdistrubucion_grupox/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.ventasdistrubucion_grupox
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.ventasdistrubucion_grupox.ui.theme.VentasDistrubucion_GrupoXTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            VentasDistrubucion_GrupoXTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    VentasDistrubucion_GrupoXTheme {
+        Greeting("Android")
+    }
+}
